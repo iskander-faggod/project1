8d566f0952cdd5180dbb1ae2e32dc9244ae10a8d
Iskander <grinerplaydark@gmail.com>
2020-02-05 15:02:07 +0500

    first commit

--- javascript/java.js
+++ javascript/java.js
@@ -2,13 +2,25 @@
 
 
 
-let incr=10 , decr=10;
+var money;
+var time;
 
-incr++;
-decr--;
+money=prompt('Введитте ваш бюджет');
+time=prompt('Дата вашего рождения ');
+var appData={
+    budjet:money,
+    timeData:time,
+    expenses:{},
+    income : [],
+    savings:false,
+};
 
-console.log(++incr);
+let a1=prompt( "Введите обязательную статью расходов в этом месяце"),
+    a2=prompt(  "Во сколько обойдется?"),
+    a3=prompt(  "Введите обязательную статью расходов в этом месяце"),
+    a4=prompt(  "Во сколько обойдется?");
 
-let IsChecked=true;
-    isClose=true;
-console.log(IsChecked || isClose);
+
+appData.expenses[a1]=a2;
+appData.expenses[a3]=a4;
+alert(appData.budjet/30);
